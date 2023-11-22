f2efdb8d3e25a6ccf96727cdf84c8d2892364a59
BDarius123 <dariusbut88@gmail.com>
2023-11-22 20:31:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mobileapp/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mobileapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        }
+}
